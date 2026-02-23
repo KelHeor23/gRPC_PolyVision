bac1dd297152b66cc7a506c5ef56ef3b373ab60a
Alex <kelheor23@gmail.com>
2026-02-23 07:13:22 +0300

    test: main client usage example read image and polygons with sending to server

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,89 +1,27 @@
-#include <grpcpp/grpcpp.h>
-
-#include <iostream>
 #include <memory>
-#include <opencv2/opencv.hpp>
-#include <optional>
-#include <string>
-#include <string_view>
+#include <opencv2/imgcodecs.hpp>
 
-#include "proto/image_analisis.grpc.pb.h"
+#include "include/impl/GrpcStreamClient.h"
+#include "include/impl/OpencvCodec.h"
+#include "include/impl/OpencvDisplay.h"
+#include "include/processing/ImageProcessingClient.h"
 
-using grpc::Channel;
-using grpc::ClientAsyncResponseReader;
-using grpc::ClientContext;
-using grpc::CompletionQueue;
-using grpc::Status;
-using image::ImageAnalysis;
+int main() {
+  auto channel = grpc::CreateChannel("localhost:50051",
+                                     grpc::InsecureChannelCredentials());
+  auto grpcClient = std::make_unique<GrpcStreamClient>(channel);
+  auto encoder = std::make_unique<OpenCVEncoder>();
+  auto display = std::make_unique<OpenCVDisplay>();
 
-/**
- * Загружает изображение из файла и возвращает его в виде cv::Mat,
- * а также заполняет переданные по ссылке параметры ширины, высоты и формата.
- *
- * @param filename путь к файлу изображения
- * @return        объект cv::Mat с изображением; если загрузка не удалась,
- * возвращается пустая матрица
- */
-std::optional<cv::Mat> loadImageInfo(const std::string_view filename) {
-  cv::Mat image = cv::imread(filename.data());
-  if (image.empty()) return std::nullopt;
+  ImageProcessingClient client(std::move(grpcClient), std::move(encoder),
+                               std::move(display));
 
-  if (!image.isContinuous()) {
-    image = image.clone();
+  cv::Mat image = cv::imread("../../../images/1.png");
+  Polygons polygons;
+  if (!polygons.loadFromFile("../../../images/1.json")) {
+    std::cerr << "Failed to load polygons from file" << std::endl;
+    return 1;
   }
-  return image;
-}
-
-class ImageAnalysisClient {
- public:
-  explicit ImageAnalysisClient(std::shared_ptr<Channel> channel)
-      : stub_(ImageAnalysis::NewStub(channel)) {}
-
-  bool UploadImage() {
-    std::optional<cv::Mat> image = loadImageInfo("1.png");
-    if (!image.has_value()) {
-      std::cerr << "Failed to load image." << std::endl;
-      return false;
-    }
-    image::Image request;
-    request.set_pixels(image->data, image->total() * image->elemSize());
-    request.set_width(image->cols);
-    request.set_height(image->rows);
-
-    image::UploadImageRequest upload_request;
-    *upload_request.mutable_image() = std::move(request);
-    image::UploadImageResponse response;
-    ClientContext context;
-    Status status = stub_->Process(&context, upload_request, &response);
-
-    if (status.ok()) {
-      const image::Image& result_image = response.result();
-      cv::Mat temp(
-          cv::Size(result_image.width(), result_image.height()), CV_8UC3,
-          const_cast<uchar*>(
-              reinterpret_cast<const uchar*>(result_image.pixels().data())));
-      cv::imshow("Uploaded Image", temp);
-      cv::waitKey(0);
-      return true;
-    } else {
-      std::cerr << "Failed to upload image: " << status.error_message()
-                << std::endl;
-      return false;
-    }
-  }
-
- private:
-  std::unique_ptr<ImageAnalysis::Stub> stub_;
-};
-
-int main(int argc, char** argv) {
-  std::string target_str = "localhost:50051";
-  ImageAnalysisClient client(
-      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  if (!client.UploadImage()) {
-    std::cerr << "Failed to upload image." << std::endl;
-  }
-
+  client.ProcessImage(image, polygons);
   return 0;
 }
