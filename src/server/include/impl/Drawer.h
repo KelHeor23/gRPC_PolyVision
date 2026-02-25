ebe453674754bb8f85cecdfdf7e9816b4eeee6ac
Alex <kelheor23@gmail.com>
2026-02-25 19:09:26 +0300

    feat: redesigned Drawer for rendering polygons and found objects

--- src/server/include/impl/Drawer.h
+++ src/server/include/impl/Drawer.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <opencv2/core.hpp>
+#include <vector>
 
 #include "interfaces/IDrawer.h"
+#include "proto/ImageAnalysis.grpc.pb.h"
+
+struct Detection;
+class IClassMapper;
 
 class Drawer : public IDrawer {
  public:
-  void draw(cv::Mat& image,
-            const std::vector<ImageDetection::Polygon>& polygons) override;
+  void drawDetections(cv::Mat& image, const std::vector<Detection>& detections,
+                      std::shared_ptr<IClassMapper> mapper) override;
+
+  void drawPolygons(
+      cv::Mat& image,
+      const std::vector<ImageDetection::Polygon>& polygons) override;
 };
