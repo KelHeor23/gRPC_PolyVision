6299a5a2a3da3e19897b76ab65b5771f4062b67c
Alex <kelheor23@gmail.com>
2026-02-25 19:40:48 +0300

    chore: add msg about starting PolygonProcessor

--- src/server/src/impl/PolygonProcessor.cpp
+++ src/server/src/impl/PolygonProcessor.cpp
@@ -5,5 +5,6 @@
 
 void PolygonProcessor::processPolygons(
     std::vector<ImageDetection::Polygon>& polygons) {
+  std::cout << "Processing " << polygons.size() << " polygons" << std::endl;
   std::ranges::sort(polygons, {}, &ImageDetection::Polygon::priority);
 }
