f87746650172796fcbb780f29adf82c8992c1a62
Alex <kelheor23@gmail.com>
2026-02-26 15:22:45 +0300

    feat: In case of equal priority, the excluding polygon is dominant

--- src/server/src/impl/PolygonProcessor.cpp
+++ src/server/src/impl/PolygonProcessor.cpp
@@ -6,5 +6,10 @@
 void PolygonProcessor::processPolygons(
     std::vector<ImageDetection::Polygon>& polygons) {
   std::cout << "Processing " << polygons.size() << " polygons" << std::endl;
-  std::ranges::sort(polygons, {}, &ImageDetection::Polygon::priority);
+  // сортируем по убыванию приоритета, а при равных приоритетах по возрастанию
+  // типа
+  std::ranges::sort(polygons, [](const auto& a, const auto& b) {
+    return a.priority() > b.priority() ||
+           (a.priority() == b.priority() && a.type() > b.type());
+  });
 }
